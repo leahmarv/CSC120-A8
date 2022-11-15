cc99299d47b6513c41ede2bf35aaba2b68c82170
leahmarv <lmarville@smith.edu>
2022-11-14 22:10:54 -0500

    Initiated Player class variables

---
+++ Player.java
@@ -0,0 +1,12 @@
+import java.util.ArrayList;
+
+public class Player {
+    String item;
+    String direction;
+    int x;
+    int y;
+    int z;
+    int size;
+    int turns;
+    ArrayList<String> moves = new ArrayList<String>();
+}
