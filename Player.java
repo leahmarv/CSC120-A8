a707a8572fc02d43690390ce702c5f53a0efe1cc
leahmarv <lmarville@smith.edu>
2022-11-14 22:17:30 -0500

    Outlined methods in Player class

--- Player.java
+++ Player.java
@@ -6,7 +6,48 @@ public class Player {
     int x;
     int y;
     int z;
-    int size;
-    int turns;
+    double size;
+    int turns = 0;
     ArrayList<String> moves = new ArrayList<String>();
+
+public void grab(String item){
+
+}
+
+public String drop(String item){
+
+}
+
+public void examine(String item){
+    System.out.println("This is a " + item);
+}
+
+public void use(String item){
+
+}
+
+public boolean walk(String direction){
+
+}
+
+public boolean fly(int x, int y){
+
+}
+
+public double shrink(){
+
+}
+
+public double grow(){
+
+}
+
+public void rest(){
+
+}
+
+public void undo(){
+
+}
+
 }
